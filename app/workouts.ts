915f45f8e07ab6b5167dd5bb9262f3892e311058
19smabtahinoor <abtahi.educxy@gmail.com>
2025-02-04 00:56:58 +0600

    init2

--- app/workouts.ts
+++ app/workouts.ts
@@ -9,20 +9,20 @@ export interface Workout {
 export const defaultWorkouts: Workout[] = [
   {
     id: 1,
-    name: "Push-ups",
-    image: "https://images.unsplash.com/photo-1598971639058-999901d1a362?w=800&auto=format&fit=crop&q=60",
-    description: "Classic upper body exercise targeting chest, shoulders, and triceps"
+    name: "1-ARM SWINGS (20 Reps) ",
+    image: "https://i.ibb.co.com/LXPCwMjB/image.png",
+    description: "Upper body exercise focusing on shoulders, chest, and back "
   },
   {
     id: 2,
-    name: "Squats",
-    image: "https://images.unsplash.com/photo-1574680096145-d05b474e2155?w=800&auto=format&fit=crop&q=60",
-    description: "Lower body exercise focusing on quadriceps, hamstrings, and glutes"
+    name: "2-Side Roatation (20 Reps)",
+    image: "https://i.ibb.co.com/Q3Pkyrjd/image.png",
+    description: "Core exercise that improves flexibility and mobility"
   },
   {
     id: 3,
-    name: "Plank",
-    image: "https://images.unsplash.com/photo-1566241142559-40e1dab266c6?w=800&auto=format&fit=crop&q=60",
-    description: "Core strengthening exercise that improves stability"
-  }
+    name: "3-Side Logs (20 Reps)",
+    image: "https://i.ibb.co.com/Jj5fhxx4/image.png",
+    description: "Core exercise that improves flexibility and mobility"
+  },
 ];
